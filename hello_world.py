67c7711d19ab358077bfb12192428bff2319a044
Caleb Hicks <hicksacaleb138@icloud.com>
2024-04-16 18:41:01 -0500

    testing for second commit

--- hello_world.py
+++ hello_world.py
@@ -8,3 +8,14 @@ print(message_2)
 
 # message = "Hello Python Crash Course reader!"
 # print(message)
+
+# print('Hello Python World!')
+
+message_1 = "Hello Python World!"
+print(message_1)
+
+message_2 = "Hello Python Crash Course World"
+print(message_2) 
+
+# message = "Hello Python Crash Course reader!"
+# print(message)
