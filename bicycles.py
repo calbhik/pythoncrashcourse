017237d51cac23912dc4718713603dbb0cde709b
Caleb Hicks <hicksacaleb138@icloud.com>
2024-04-16 21:00:01 -0500

    party misshap

---
+++ bicycles.py
@@ -0,0 +1,17 @@
+# Square brackets ([]) indicate a list, and individual elements in the list are separated by commas. Example below
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[0].upper())
+
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[1])
+# print(bicycles[3])
+
+# Python has a special syntax for accessing the last element in a list. If you ask for the item at index -1, Python always returns the last item in the list:
+# bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles[-1])
+
+# Using the fstring to pull from the list to print a message
+#bicycle = ['trek', 'cannondale', 'redline', 'specialized']
+# message = f'My first bicycle was a {bicycle[0].title()}.'
+# print(message)
